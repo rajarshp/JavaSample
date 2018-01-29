7d5ea63aed151ab76e74b6f19325366fea82b527
Joker <Joker@192.168.1.9>
2018-01-29 20:47:38 +0530

    transaction seems working

---
+++ springtransaction/src/main/java/com/example/springtransaction/Account.java
@@ -0,0 +1,26 @@
+package com.example.springtransaction;
+
+public class Account 
+{
+	private int acnumber;
+	private int userid;
+	
+	public int getAcnumber() 
+	{
+		return acnumber;
+	}
+	public void setAcnumber(int acnumber) 
+	{
+		this.acnumber = acnumber;
+	}
+	public int getUserid()
+	{
+		return userid;
+	}
+	public void setUserid(int userid) 
+	{
+		this.userid = userid;
+	}
+	
+	
+}
