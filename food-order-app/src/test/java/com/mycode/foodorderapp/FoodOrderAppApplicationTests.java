039c5bd1e1a23a5bb464ae5f5f21b03aafabb750
Rajarshi Pain <13257922+rajarshp@users.noreply.github.com>
2023-03-19 16:27:06 -0500

    new project - food order app backend
    new project - food order app backend

---
+++ food-order-app/src/test/java/com/mycode/foodorderapp/FoodOrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycode.foodorderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
